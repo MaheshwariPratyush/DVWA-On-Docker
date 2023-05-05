39f8f5cc94c5bbc28d0b16805227f2c820bee5e2
Swastik Kataria <76050496+ProSwastik@users.noreply.github.com>
2023-05-05 23:41:43 +0530

    Update Source Code For a Potential Error of Execution In The Script

--- dvwa-docker-install.sh
+++ dvwa-docker-install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-sudo su
-apt install docker.io -y
-docker pull vulnerables/web-dvwa
+echo "Enter Your Sudo Password : "
+read PASS
+echo "$PASS" | sudo -S apt install docker.io -y
+echo "$PASS" | sudo -S docker pull vulnerables/web-dvwa
 touch dvwa
 echo "sudo docker run --rm -it -p 8080:8080 vulnerables/web-dvwa" | tee dvwa
-chmod +x dvwa
-cp dvwa /bin
-cp dvwa /usr/bin
-cp dvwa /usr/local/bin
+echo "$PASS" | sudo -S chmod +x dvwa
+echo "$PASS" | sudo -S cp dvwa /bin
+echo "$PASS" | sudo -S cp dvwa /usr/bin
+echo "$PASS" | sudo -S cp dvwa /usr/local/bin
 echo "DVWA has been successfully installed on your machine, run it using: dvwa"
